f1f166335cfe16087af421edd270c015a8a1ace6
andyjohn23 <andrewnick906@gmail.com>
2020-08-26 23:47:07 +0300

    updated files

--- js/script.js
+++ js/script.js
@@ -1,5 +1,50 @@
 $(document).ready(function(){
     $(".card1").mouseover(function(){
-     $(this).show("p");
+    $("#layer1").show();
+    }).mouseout(function(){
+    $("#layer1").hide();
     });
+
+      $(".card2").mouseover(function(){
+        $("#layer2").show();
+       }).mouseout(function(){
+        $("#layer2").hide();
+       });
+
+       $(".card3").mouseover(function(){
+        $("#layer3").show();
+       }).mouseout(function(){
+        $("#layer3").hide();
+       });
+
+       $(".card4").mouseover(function(){
+        $("#layer4").show();
+       }).mouseout(function(){
+        $("#layer4").hide();
+       });
+
+       $(".card5").mouseover(function(){
+        $("#layer5").show();
+       }).mouseout(function(){
+        $("#layer5").hide();
+       });
+
+       $(".card6").mouseover(function(){
+        $("#layer6").show();
+       }).mouseout(function(){
+        $("#layer6").hide();
+       });
+
+       $(".card7").mouseover(function(){
+        $("#layer7").show();
+       }).mouseout(function(){
+        $("#layer7").hide();
+       });
+
+       $(".card8").mouseover(function(){
+        $("#layer8").show();
+       }).mouseout(function(){
+        $("#layer8").hide();
+       });
+
 });
